ba623c2e46d46d40f1de27058ee464d7e98a122b
unknown <jagadeeshganta7@gmail.com>
2025-11-08 20:08:43 +0530

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
   title: "Portfolio of JAGADEESH GANTA - Software Developer",
   description:
     "This is the portfolio of JAGADEESH GANTA. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
